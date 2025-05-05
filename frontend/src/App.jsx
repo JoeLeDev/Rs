3e16f091a52cd4c959d67ea0319aadf9b82d93cf
Jonathan Luembe <jonathanluembe@yahoo.com>
2025-05-05 12:08:41 +0200

    Multiple fix

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
